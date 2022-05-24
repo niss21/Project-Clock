8ef553c20ac9b00fcf0ce4760bec620aff24ec0c
niss21 <102400748+niss21@users.noreply.github.com>
2022-05-24 18:56:46 +0545

    Update file.js
    just changed the ui with js!!!

--- file.js
+++ file.js
@@ -1,9 +1,11 @@
 let a;
 let date;
 let time;
+const option= { weekday: 'long', year: 'numeric', month: 'long' ,day: 'numeric'};
 setInterval(() => {
     a=new Date();
-    date=a.toLocaleDateString();
+//     date=a.toLocaleDateString();
+     date=a.toLocaleDateString(undefined,option);
     time=a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
     document.getElementById('time').innerHTML = time + "  on  " + date;
 }, 1000);
